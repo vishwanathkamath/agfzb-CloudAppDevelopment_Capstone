13d201f9d7f59d9602888603a1f75754e0a395f1
Vishwanath Kamath Pethri <vishwanathkamath@gmail.com>
2021-07-24 18:17:27 +0000

    Commits

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+#from .models import related models
+from .models import CarModel
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, post_request
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 import logging
 import json
 
